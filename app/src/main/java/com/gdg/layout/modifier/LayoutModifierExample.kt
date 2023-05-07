4bf430b3f073862673de1cba6b8df28d0e96b9c8
NGARG3 <neha.garg@volvocars.com>
2023-05-07 19:25:49 +0200

    use Constraints fixedWidth in LayoutModifierExample.kt

--- app/src/main/java/com/gdg/layout/modifier/LayoutModifierExample.kt
+++ app/src/main/java/com/gdg/layout/modifier/LayoutModifierExample.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
 import com.gdg.ui.theme.GDGTheme
 
@@ -75,16 +76,12 @@ fun LayoutModifierExample() {
         Text("MyThridBasicColumn",
             Modifier
                 .layout { measurable, constraints ->
-                    val placeable = measurable.measure(
-                        constraints.copy(
-                            maxWidth = constraints.maxWidth + 80.dp.roundToPx()
-                        )
-                    )
+                    val placeable = measurable.measure(Constraints.fixedWidth(constraints.maxWidth - 150.dp.roundToPx()))
+
                     layout(placeable.width, placeable.height) {
-                        placeable.place(0, 0)
+                        placeable.place(100, 0)
                     }
                 }
-                .fillMaxWidth()
                 .background(Color.DarkGray))
 
     }
